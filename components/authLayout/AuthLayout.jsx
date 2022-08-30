d77c32dfb49c1818a06bc09cbeef92e801a80cfc
Cradoe <me@ismailobadimu.com>
2022-08-31 00:18:04 +0100

    fixed babelnext issue

--- components/authLayout/AuthLayout.jsx
+++ components/authLayout/AuthLayout.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { logo } from "assets";
 import styles from '../../styles/auth.module.scss'
 import { Layout } from "components";
